4e54efcae28701b56ed25115df7c4d8ba927985e
l3ycle <github@b.fam-reichel.de>
2020-09-20 12:50:28 +0200

    better greeting text

--- js/greeting.js
+++ js/greeting.js
@@ -3,17 +3,17 @@ var Hr = today.getHours();
 
 // Here you can change the greetings and your name
 var name = 'Ben';
-var lateTxt = 'Go to Sleep! ';
-var morningTxt = 'Good morning! ';
+var lateTxt = 'Go to Sleep ';
+var morningTxt = 'Good morning ';
 var afterTxt = 'Good afternoon ';
 var evenTxt = 'Good evening ';
 
 if (Hr >= 0 && Hr < 6) {
-    document.getElementById('greetings').innerText = lateTxt + name;
-    document.getElementById('title_content').innerText = lateTxt + name;
+    document.getElementById('greetings').innerText = lateTxt + name + "!";
+    document.getElementById('title_content').innerText = lateTxt + name +  "!";
 } else if (Hr >= 6 && Hr < 12) {
-    document.getElementById('greetings').innerText = morningTxt + name;
-    document.getElementById('title_content').innerText = morningTxt + name;
+    document.getElementById('greetings').innerText = morningTxt + name + "!";
+    document.getElementById('title_content').innerText = morningTxt + name + "!";
 } else if (Hr >= 12 && Hr < 17) {
     document.getElementById('greetings').innerText = afterTxt + name;
     document.getElementById('title_content').innerText = afterTxt + name;
