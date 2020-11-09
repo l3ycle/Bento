813b3710d80b5c7dcc923b3ef73556e96f796e01
l3ycle <github@b.fam-reichel.de>
2020-11-09 08:58:24 +0100

    some whitspace magically appears

--- js/time.js
+++ js/time.js
@@ -24,13 +24,13 @@ function displayClock() {
     var min = (mins = ('0' + d.getMinutes()).slice(-2));
     var hh = d.getHours();
     var ampm = '';
-    
+
     if (format_12hour) {
         ampm = hh >= 12 ? ' pm' : ' am';
         hh = hh % 12;
         hh = hh ? hh : 12; //show mod 0 as 12
     }
-  
+
     document.getElementById('hour').innerText = hh;
     document.getElementById('separator').innerHTML = ' : ';
     document.getElementById('minutes').innerText = min + ampm;
